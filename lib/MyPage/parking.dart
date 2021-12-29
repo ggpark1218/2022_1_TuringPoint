302b0d368e4e806a2cdb11823e5d81bffdaf39d0
GwonYuri <21900048@handong.edu>
2021-12-29 14:53:26 +0900

    전체 페이지 구성1

---
+++ lib/MyPage/parking.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Parking extends StatefulWidget {
+  const Parking({Key? key}) : super(key: key);
+
+  @override
+  _ParkingState createState() => _ParkingState();
+}
+
+class _ParkingState extends State<Parking> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/app.dart
@@ -0,0 +1,4 @@
+import 'main.dart';
+import 'package:flutter/material.dart';
+
+
