87785dc0b41e4c0ed13c3355a019dbc66208999d
dkdkdk <21900646@handong.edu>
2021-12-29 15:06:22 +0900

    page 만들기

---
+++ lib/mypage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class mypage extends StatefulWidget {
+  const mypage({Key? key}) : super(key: key);
+
+  @override
+  _mypageState createState() => _mypageState();
+}
+
+class _mypageState extends State<mypage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
