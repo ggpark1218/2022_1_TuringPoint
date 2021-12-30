1b509105d8ac6c7a8468836fddd4fe4b8c2b36b7
parkgyugyeong <ggpark1218>
2021-12-30 19:21:36 +0900

    12/30 precheck 추가

---
+++ lib/precheck.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class precheck extends StatefulWidget {
+  const precheck({Key? key}) : super(key: key);
+
+  @override
+  _precheckState createState() => _precheckState();
+}
+
+class _precheckState extends State<precheck> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
