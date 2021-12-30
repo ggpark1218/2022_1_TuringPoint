d76a94a92b616b3f21bf099be7faf2ba4ba304d9
parkgyugyeong <ggpark1218>
2021-12-30 14:48:53 +0900

    12/30 app theme 수정

--- lib/theme/app_text_styles.dart
+++ lib/theme/app_text_styles.dart
@@ -2,17 +2,41 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppTextStyle{
-  static TextStyle headline1 = GoogleFonts.montserrat(
-    fontWeight: FontWeight.w700,
-    fontSize: 24,
-    height: 32/24, //폰트 사이즈로 꼭 나눠줘야함
-    letterSpacing: -0.8
+  static TextStyle headline1 = const TextStyle(
+    fontFamily: 'Millimetre-Bold.otf',
+    fontWeight: FontWeight.w800,
+    fontSize: 64,
+    height: 24/64, //폰트 사이즈로 꼭 나눠줘야함
+    letterSpacing: 0.15
   );
 
-  static TextStyle body1 = GoogleFonts.montserrat(
-      fontWeight: FontWeight.w700,
-      fontSize: 24,
-      height: 32/24,
-      letterSpacing: -0.8
+  static TextStyle headline2 = const TextStyle(
+      fontFamily: 'Millimetre-Bold.otf',
+      fontWeight: FontWeight.w800,
+      fontSize: 48,
+      height: 56/48, //폰트 사이즈로 꼭 나눠줘야함
+      letterSpacing: 0.15
+  );
+
+  static TextStyle headline3 = const TextStyle(
+      fontFamily: 'Millimetre-Regular.otf',
+      fontWeight: FontWeight.w500,
+      fontSize: 36,
+      height: 20/36, //폰트 사이즈로 꼭 나눠줘야함
+      letterSpacing: 0.25
+  );
+
+  static TextStyle subtitle1 = GoogleFonts.roboto(
+      fontWeight: FontWeight.w400,
+      fontSize: 30,
+      height: 42/30,
+      letterSpacing: 0.15
+  );
+
+  static TextStyle body1 = GoogleFonts.roboto(
+      fontWeight: FontWeight.w500,
+      fontSize: 28,
+      height: 50/28,
+      letterSpacing: 0.15
   );
 }
