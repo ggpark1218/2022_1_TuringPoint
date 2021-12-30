581b539818d0f076d3a5dbed5a0e7b388335b757
dkdkdk <21900646@handong.edu>
2021-12-30 09:43:56 +0900

    ...

--- lib/mypage.dart
+++ lib/mypage.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'MyPage/announcement.dart';
+import 'MyPage/care.dart';
+import 'MyPage/customer_service.dart';
+import 'MyPage/FAQ.dart';
+import 'MyPage/parking.dart';
+import 'MyPage/profile.dart';
+import 'MyPage/setting.dart';
+import 'login.dart';
 
 class mypage extends StatefulWidget {
   const mypage({Key? key}) : super(key: key);
@@ -10,6 +19,56 @@ class mypage extends StatefulWidget {
 class _mypageState extends State<mypage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        child: Expanded(
+          child: Column(
+            children: [
+              Card(
+                child: InkWell(
+                  onTap: () {
+                    Get.to(Announcement());
+                  },
+                  child: Text('자주 묻는 질문')
+                ),
+              ),
+              Card(
+                child: InkWell(
+                    onTap: () {
+                      Get.to(Care());
+                    },
+                    child: Text('고객센터')
+                ),
+              ),
+              Card(
+                child: InkWell(
+                    onTap: () {
+                      Get.to(CustomerService());
+                    },
+                    child: Text('공지사항')
+                ),
+              ),
+              Card(
+                child: InkWell(
+                    onTap: () {
+                      Get.to(Setting());
+                    },
+                    child: Text('앱 설정')
+                ),
+              ),
+              Card(
+                child: InkWell(
+                    onTap: () {
+                      Get.offAll(() => login());
+                    },
+                    child: Text('로그아웃')
+                ),
+              ),
+              ]
+            ),
+        ),
+      ),
+    );
   }
 }
